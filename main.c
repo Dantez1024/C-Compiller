035673c80a80841ff168dff573eecf5a11d9ece5
Dantez1024 <ndung'u.daniel@s.karu.ac.ke>
2022-05-06 13:44:41 +0300

    Finished lexical analyzer part 1

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 //Author  Leinad Cybre
+//A C lexical analyzer
 //Program to scan lexemes from C code
+
 
 #include <stdio.h> //standard I/O
 #include <string.h> //to allow input to be line of code
 #include <stdbool.h> //boolean function
 #include <stdlib.h>
+#include <ctype.h>
 
-bool isDelimeter = false, isOperator = false, validID = true;
+int isDelimeter = 0, isOperator = 0, validID = 0, isKeyWord = 0;
 
 
 
@@ -36,34 +39,75 @@ bool checkChar(char ch[],char ch2)
     }
 }
 
-//Returns true if the character is delimeter
-bool setBoolValues(char ch,char ch2[])
+int checkIdentifier(char buffer)
 {
-    char delimeter [] ={'/','+','-','*',']','['};
+    int flag = 0;
+    if(!isdigit(buffer) )
+    {
+
+       flag = 1;
+    }
+
+    return flag;
+}
+
+int checkKeyword(char buffer[])
+{
+    int flag = 0, i;
+    char keywords[32][10] = {"auto","break","case","char","const","continue","default",
+"do","double","else","enum","extern","float","for","goto",
+"if","int","long","register","return","short","signed",
+"sizeof","static","struct","switch","typedef","union",
+"unsigned","void","volatile","while"};
+
+for(i = 0; i < 32; ++i) {
+    if(strcmp(keywords[i], buffer) == 0) {
+        flag  = 1;
+        break;
+    }
+}
+return flag;
+
+
+}
+
+//Returns true if the character is delimeter
+void setBoolValues(char ch,char ch2[])
+{
+    if(isKeyWord){
+        checkKeyword(ch2);
+    }else
+     {
+
+    char delimeter [] ={';',':'};
     char operand [] = {'+', '-', '/', '*','='};
     char badIds [] = {'1', '2', '3', '4', '5', '6', '7', '8', '9','0'};
 
-    bool isDel, isOp, validID2;
+
+
 
     if(checkChar(delimeter, ch)) {
-         isDelimeter = true;
+         isDelimeter = 1;
     }
-    if(checkChar(operand, ch)) {
-         isOperator = true;
-    }
-
-
-    if(checkChar(badIds, ch) || checkChar(delimeter, ch)) {
-         validID = false;
+    if(checkChar(operand, ch) && !isDelimeter ) {
+         isOperator = 1;
     }
 
 
-    return true;
+    if(checkIdentifier(ch) && !isDelimeter && !isOperator)
+       {
+           validID = 1;
+       }
+
+
+
+
+     }
 
 }
 void resetBoolValues()
 {
-    bool isDelimeter=false, isOperator = false, validID = true;
+     isDelimeter=0; isOperator = 0; validID = 0; isKeyWord = 0;
 }
 
 int getSize(char ch[])
@@ -106,16 +150,25 @@ int main()
 
     int codeLn = getSize(str);
 
-    printf("Code has %d \n",codeLn);
+    int keyword = checkKeyword(str);
 
-    for(int i = 0; i<codeLn; i++)
+    if(!keyword){
+        for(int i = 0; i<codeLn; i++)
     {
+
         setBoolValues(str[i], str);
 
-        printf("%d. %c Code Results : \n IsDelimeter => %i \n IsOperator => %i \n Valid Identifier => %i \n",i,str[i],isDelimeter,isOperator,validID);
+        printf("%d. %c Codes Results : \n IsDelimeter => %d \n IsOperator => %d \n Valid Identifier => %d \n \n",i,str[i],isDelimeter,isOperator,validID);
 
         resetBoolValues();
+
+
     }
+
+    }else {
+    printf("%s is a keyword",str);
+    }
+
 
 
 
